eb6f8c9cbe8720b6c09b142fa7bf92c9332c7921
Kihyun Ryu <khryu0610@gmail.com>
2025-01-04 17:16:21 +0900

    feat: 컨텐츠 구조 결정
    - depth 2: category - subCategory

---
+++ apps/blog/constants/categories.ts
@@ -0,0 +1,8 @@
+export const CATEGORIES = ["fe", "be", "algo", "cs"] as const;
+
+export const SUB_CATEGORIES = {
+  fe: ["react", "vue", "next.js"],
+  be: ["spring"],
+  algo: ["dp"],
+  cs: ["computer architecture", "operating system"],
+} as const;
