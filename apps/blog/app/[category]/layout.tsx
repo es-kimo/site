7fcd8216837de3f5e43ae0d450a9bb35180d9911
Kihyun Ryu <khryu0610@gmail.com>
2025-01-10 16:16:33 +0900

    feat: 노트리스트에 노트카드 적용

--- apps/blog/app/[category]/layout.tsx
+++ apps/blog/app/[category]/layout.tsx
@@ -1,5 +1,8 @@
 import NavigationTab from "@/components/navigation-tab";
+import { NoteCard } from "@/components/note-card";
+import { getSlugsByCategoryAndSub, getSubCategoriesByCategory } from "@/constants/notes";
 import { categoryParams } from "@/constants/params";
+import { CategoryParams } from "@/constants/params.types";
 import { isCategory } from "@/lib/type-guards";
 import { t } from "@/locales/translate";
 import { notFound } from "next/navigation";
@@ -13,16 +16,24 @@ export default async function Layout({
   params,
 }: Readonly<{
   children: React.ReactNode;
-  params: Promise<{ category: string }>;
+  params: Promise<CategoryParams>;
 }>) {
-  const { category } = await params;
-
+  const { category, sub } = await params;
+  const subs = sub ? [sub] : [...(await getSubCategoriesByCategory(category))];
   return (
-    <main className="pt-6 pb-[10vh] px-8 sm:pt-[80px] sm:pb-[20vh] space-y-6">
+    <main className="pt-6 pb-[10vh] px-8 sm:pt-[80px] sm:pb-[20vh] space-y-10">
       {isCategory(category) ? (
         <>
           <h2 className="font-bold text-3xl">{t(category)}</h2>
           <NavigationTab category={category} />
+          <article>
+            <h3 className="sr-only">아티클</h3>
+            <ul className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+              {/* TODO: sort by created date */}
+              {subs.map(async (sub) => (await getSlugsByCategoryAndSub(category, sub)).map((slug) => <li key={slug}>{<NoteCard category={category} sub={sub} slug={slug} />}</li>))}
+            </ul>
+          </article>
+
           {children}
         </>
       ) : (
--- apps/blog/constants/params.types.ts
+++ apps/blog/constants/params.types.ts
@@ -1,3 +1,3 @@
-export type CategoryParams = { category: string };
-export type SubParams = { category: string; sub: string };
+export type CategoryParams = { category: string; sub?: string; slug?: string };
+export type SubParams = { category: string; sub: string; slug?: string };
 export type SlugParams = { category: string; sub: string; slug: string };
