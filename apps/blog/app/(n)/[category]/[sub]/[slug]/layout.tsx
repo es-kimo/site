0b9626c37c5310a5dcd2ad667740284e00decc02
Kihyun Ryu <khryu0610@gmail.com>
2025-01-10 16:10:34 +0900

    chore: breadcrumb props 일반화

--- apps/blog/app/(n)/[category]/[sub]/[slug]/layout.tsx
+++ apps/blog/app/(n)/[category]/[sub]/[slug]/layout.tsx
@@ -1,5 +1,6 @@
 import { BreadCrumb } from "@/components/breadcrumb";
 import { slugParams } from "@/constants/params";
+import { SlugParams } from "@/constants/params.types";
 
 /** route group이 달라서 부모로부터 static param을 받을 수 없음 */
 export function generateStaticParams() {
@@ -16,15 +17,19 @@ export async function generateMetadata() {
 
 export default async function Layout({
   children,
+  params,
 }: Readonly<{
   children: React.ReactNode;
+  params: Promise<SlugParams>;
 }>) {
+  const { category, sub } = await params;
+
   return (
     <section className="grid grid-cols-[minmax(0.875rem,_1fr)_minmax(auto,_708px)_minmax(0.875rem,_1fr)] sm:grid-cols-[minmax(96px,_3fr)_minmax(auto,_708px)_minmax(96px,_1fr)] pt-6 pb-[10vh] sm:pt-[80px] sm:pb-[20vh] w-full transition-all">
       <aside className="hidden lg:block sticky top-[80px] left-3 border-y-2 w-[180px] h-fit">
         <dl className="border-b-[1px] border-muted px-[7px]">
           <dd className="py-[6px]">
-            <BreadCrumb />
+            <BreadCrumb category={category} sub={sub} />
           </dd>
         </dl>
         <dl className="flex gap-4 text-xs px-[7px] py-[9px]">
@@ -33,7 +38,7 @@ export default async function Layout({
         </dl>
       </aside>
       <article className="col-start-2">
-        <BreadCrumb className="lg:hidden text-muted-foreground mb-1" />
+        <BreadCrumb className="lg:hidden text-muted-foreground mb-1" category={category} sub={sub} />
         {children}
       </article>
     </section>
