8768af22a376997dfa42bbec1fed77bd86680cf6
Kihyun Ryu <khryu0610@gmail.com>
2025-03-12 00:43:00 +0900

    feat: 메뉴 전체보기에 아코디언 추가

--- apps/yjshd/components/TheHeader.tsx
+++ apps/yjshd/components/TheHeader.tsx
@@ -1,14 +1,16 @@
-import { getCategories } from "@workspace/common/structure/notes";
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@workspace/ui/components/accordion";
+import { getCategories, getSubCategoriesByCategory } from "@workspace/common/structure/notes";
 import { Button } from "@workspace/ui/components/button";
 import { ModeToggle } from "@workspace/ui/components/mode-toggle";
 import { Sheet, SheetClose, SheetContent, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from "@workspace/ui/components/sheet";
 import { Menu } from "lucide-react";
 import { Link } from "next-view-transitions";
+import { removeNumbering } from "@workspace/common/lib/file-system";
 
 const H1 = "연세정성내과의원";
 
 export const TheHeader = async () => {
-  const categories = (await getCategories()).map((category) => category.split(".").pop() || "");
+  const categories = await getCategories();
 
   return (
     <header className="grid grid-cols-5 py-4 sticky top-0 z-10 bg-background">
@@ -18,7 +20,7 @@ export const TheHeader = async () => {
       <nav className="col-span-3 hidden sm:block">
         {categories.map((category) => (
           <NavigationButton key={category} href={`/${category}`}>
-            {category}
+            {removeNumbering(category)}
           </NavigationButton>
         ))}
       </nav>
@@ -43,11 +45,14 @@ export const NavigationButton = ({ href, children }: { href: string; children: R
   );
 };
 
-export const AllCategoriesSheetRight = ({ categories }: { categories: string[] }) => {
+export const AllCategoriesSheetRight = async ({ categories }: { categories: string[] }) => {
+  const subs = await Promise.all(categories.map(async (category) => await getSubCategoriesByCategory(category)));
+  const hasSubCategories = subs.map((sub) => !!sub.length);
+
   return (
     <Sheet>
       <SheetTrigger asChild>
-        <Button variant="ghost" className="text-lg text-neutral-400 [&.active]:text-accent-foreground">
+        <Button variant="ghost" className="text-lg [&.active]:text-accent-foreground">
           <Menu />
           전체보기
         </Button>
@@ -58,17 +63,45 @@ export const AllCategoriesSheetRight = ({ categories }: { categories: string[] }
         </SheetHeader>
         <nav className="flex flex-col gap-2">
           <SheetClose asChild>
-            <Button asChild variant="ghost" className="text-xl text-neutral-400 [&.active]:text-accent-foreground justify-start">
+            <Button asChild variant="ghost" className="text-lg [&.active]:text-accent-foreground justify-start border-b py-3 box-content">
               <Link href="/">홈</Link>
             </Button>
           </SheetClose>
-          {categories.map((category) => (
-            <SheetClose asChild key={category}>
-              <Button asChild variant="ghost" className="text-xl text-neutral-400 [&.active]:text-accent-foreground justify-start">
-                <Link href={category}>{category}</Link>
-              </Button>
-            </SheetClose>
-          ))}
+          <Accordion type="single" collapsible>
+            {categories
+              .filter((_, idx) => hasSubCategories[idx])
+              .map((category, idx) => (
+                <AccordionItem value={`item-${idx + 1}`} key={`item-${idx + 1}-${category}`}>
+                  <AccordionTrigger>
+                    <SheetClose asChild key={category}>
+                      <Button asChild variant="ghost" className="text-lg [&.active]:text-accent-foreground justify-start">
+                        <Link replace href={`/${category}`}>
+                          {removeNumbering(category)}
+                        </Link>
+                      </Button>
+                    </SheetClose>
+                  </AccordionTrigger>
+                  <AccordionContent className="flex flex-col">
+                    {subs[idx]?.map((sub) => (
+                      <SheetClose asChild key={sub}>
+                        <Button asChild variant="ghost" className="text-neutral-400 text-lg [&.active]:text-accent-foreground justify-start">
+                          <Link href={`/${category}/${sub}`}>{removeNumbering(sub)}</Link>
+                        </Button>
+                      </SheetClose>
+                    ))}
+                  </AccordionContent>
+                </AccordionItem>
+              ))}
+          </Accordion>
+          {categories
+            .filter((_, idx) => !hasSubCategories[idx])
+            .map((category, idx) => (
+              <SheetClose asChild key={`item-${idx + 1}-${category}`}>
+                <Button asChild variant="ghost" className="text-lg [&.active]:text-accent-foreground justify-start  border-b py-3 box-content">
+                  <Link href={`/${category}`}>{removeNumbering(category)}</Link>
+                </Button>
+              </SheetClose>
+            ))}
         </nav>
         <SheetFooter>
           <SheetClose>{H1}</SheetClose>
