5e74cd9ec6df810f2fc7f688ce9a28d0b1cdd992
Kihyun Ryu <khryu0610@gmail.com>
2025-03-11 18:59:18 +0900

    feat: 메인 헤더 작성

--- apps/yjshd/app/layout.tsx
+++ apps/yjshd/app/layout.tsx
@@ -1,30 +1,35 @@
-import { Geist, Geist_Mono } from "next/font/google"
+import { Geist, Geist_Mono } from "next/font/google";
 
-import "@workspace/ui/globals.css"
-import { Providers } from "@/components/providers"
+import { Providers } from "@/components/providers";
+import { TheHeader } from "@/components/TheHeader";
+import "@workspace/ui/globals.css";
+import { ViewTransitions } from "next-view-transitions";
 
 const fontSans = Geist({
   subsets: ["latin"],
   variable: "--font-sans",
-})
+});
 
 const fontMono = Geist_Mono({
   subsets: ["latin"],
   variable: "--font-mono",
-})
+});
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${fontSans.variable} ${fontMono.variable} font-sans antialiased `}
-      >
-        <Providers>{children}</Providers>
-      </body>
-    </html>
-  )
+    <ViewTransitions>
+      <html lang="en" suppressHydrationWarning>
+        <body className={`${fontSans.variable} ${fontMono.variable} font-sans antialiased `}>
+          <Providers>
+            <TheHeader />
+            {children}
+          </Providers>
+        </body>
+      </html>
+    </ViewTransitions>
+  );
 }
