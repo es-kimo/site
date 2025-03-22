0d023b3a1282b242c2195bf46ee85dde2514373b
Kihyun Ryu <khryu0610@gmail.com>
2025-03-22 23:27:25 +0900

    refactor: 컨텐츠 구조 관련 유틸 함수 모음

---
+++ packages/common/src/structure/utils.ts
@@ -0,0 +1,15 @@
+import { getFolderNames } from "@workspace/common/lib/file-system";
+import path from "path";
+
+export const ENTRY_POINT = "content";
+
+export const CONTENT_PATH = path.join(process.cwd(), ENTRY_POINT);
+
+/** 카테고리 */
+export const getCategories = async () => await getFolderNames(CONTENT_PATH);
+
+/** 서브 카테고리 */
+export const getSubCategoriesByCategory = async (category: string) => getFolderNames(path.join(CONTENT_PATH, category));
+
+/** 글 제목 */
+export const getSlugsByCategoryAndSubCategory = async (category: string, subCategory: string) => getFolderNames(path.join(CONTENT_PATH, category, subCategory));
