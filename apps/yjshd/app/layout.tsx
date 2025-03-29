94d4ec6422843a9455d328806ed18555678b95c7
Kihyun Ryu <khryu0610@gmail.com>
2025-03-29 10:28:46 +0900

    feat: 글로벌 레이아웃에 max-width 추가

--- apps/yjshd/app/layout.tsx
+++ apps/yjshd/app/layout.tsx
@@ -1,9 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
 
-import { TheHeader } from "@/components/TheHeader";
 import "@workspace/ui/globals.css";
 import { ViewTransitions } from "next-view-transitions";
-import { CategoryParams } from "@workspace/common/structure/params.types";
+import { Metadata } from "next";
 
 const fontSans = Geist({
   subsets: ["latin"],
@@ -17,17 +16,14 @@ const fontMono = Geist_Mono({
 
 export default function RootLayout({
   children,
-  params,
 }: Readonly<{
   children: React.ReactNode;
-  params: Promise<CategoryParams>;
 }>) {
   return (
     <ViewTransitions>
       <html lang="en" suppressHydrationWarning>
         <body className={`${fontSans.variable} ${fontMono.variable} font-sans antialiased `}>
-          <TheHeader params={params} />
-          {children}
+          <div className="max-w-blog mx-auto">{children}</div>
         </body>
       </html>
     </ViewTransitions>
