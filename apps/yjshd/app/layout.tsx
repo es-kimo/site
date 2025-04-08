b0e2365a5d9affa1360f217c537f9c16e1b31cd1
Kihyun Ryu <khryu0610@gmail.com>
2025-04-08 15:41:40 +0900

    feat: 글로벌 폰트 Noto Sans로 변경

--- apps/yjshd/app/layout.tsx
+++ apps/yjshd/app/layout.tsx
@@ -1,18 +1,5 @@
-import { Geist, Geist_Mono } from "next/font/google";
-
 import "@workspace/ui/globals.css";
 import { ViewTransitions } from "next-view-transitions";
-import { Metadata } from "next";
-
-const fontSans = Geist({
-  subsets: ["latin"],
-  variable: "--font-sans",
-});
-
-const fontMono = Geist_Mono({
-  subsets: ["latin"],
-  variable: "--font-mono",
-});
 
 export default function RootLayout({
   children,
@@ -22,7 +9,7 @@ export default function RootLayout({
   return (
     <ViewTransitions>
       <html lang="en" suppressHydrationWarning>
-        <body className={`${fontSans.variable} ${fontMono.variable} font-sans antialiased `}>
+        <body className={`antialiased`}>
           <div className="max-w-blog mx-auto">{children}</div>
         </body>
       </html>
