65094314a770156a3201a902c7e99edf3862cb9d
Kihyun Ryu <khryu0610@gmail.com>
2025-06-19 19:10:32 +0900

    fix: await params

--- apps/blog/app/(dynamic)/ossu/archive/[lectureName]/page.tsx
+++ apps/blog/app/(dynamic)/ossu/archive/[lectureName]/page.tsx
@@ -2,21 +2,15 @@ import { getLectureCourses } from "@/lib/github";
 import { IndexList } from "@workspace/ui/templates/IndexList";
 import { Link } from "next-view-transitions";
 
-export default async function LecturePage({ params }: { params: { lectureName: string } }) {
-  const courses = await getLectureCourses(params.lectureName);
+export default async function LecturePage({ params }: { params: Promise<{ lectureName: string }> }) {
+  const { lectureName } = await params;
+  const courses = await getLectureCourses(lectureName);
   return (
     <IndexList>
-      <IndexList.Header>{params.lectureName}</IndexList.Header>
+      <IndexList.Header>{lectureName}</IndexList.Header>
       <IndexList.Grid>
         {courses.map(({ name, readmeContent }) => (
-          <IndexList.Item
-            key={name}
-            Link={Link}
-            item={name}
-            href={`/ossu/archive/${params.lectureName}/${name}`}
-            title={name}
-            hashtags={[readmeContent.substring(0, readmeContent.indexOf("\n")).trim()]}
-          />
+          <IndexList.Item key={name} Link={Link} item={name} href={`/ossu/archive/${lectureName}/${name}`} title={name} hashtags={[readmeContent.substring(0, readmeContent.indexOf("\n")).trim()]} />
         ))}
       </IndexList.Grid>
     </IndexList>
