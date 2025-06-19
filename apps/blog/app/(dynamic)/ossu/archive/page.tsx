bf426606eef5da9497f1e4ab61b294119915a04a
Kihyun Ryu <khryu0610@gmail.com>
2025-06-19 18:50:58 +0900

    feat: 인덱스 페이지 작성

--- apps/blog/app/(dynamic)/ossu/archive/page.tsx
+++ apps/blog/app/(dynamic)/ossu/archive/page.tsx
@@ -1,9 +1,23 @@
-import { getOssuLectureList } from "@/lib/github";
+import { getOssuLectureList, getLectureTopics } from "@/lib/github";
+import { zip } from "@workspace/common/lib/array-utils";
+import { IndexList } from "@workspace/ui/templates/IndexList";
+import { Link } from "next-view-transitions";
 
 export default async function ArchivePage() {
   const lectures = await getOssuLectureList();
-
-  console.log(lectures);
-
-  return <div>Archive</div>;
+  const topics = await Promise.all(
+    lectures.map((lecture) => {
+      return getLectureTopics(lecture);
+    })
+  );
+  return (
+    <IndexList>
+      <IndexList.Header>OSSU Archive</IndexList.Header>
+      <IndexList.Grid>
+        {zip(lectures, topics).map(([lecture, topics]) => (
+          <IndexList.Item key={lecture} Link={Link} item={lecture} href={`/ossu/archive/${lecture}`} title={lecture} hashtags={topics} />
+        ))}
+      </IndexList.Grid>
+    </IndexList>
+  );
 }
