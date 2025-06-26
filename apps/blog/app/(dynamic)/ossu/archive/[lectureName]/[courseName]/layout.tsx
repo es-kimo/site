2aff3d544721b586b7180f627232d521fa4c58ab
Kihyun Ryu <khryu0610@gmail.com>
2025-06-26 21:11:41 +0900

    fix: breadcrumb 스타일 통일

--- apps/blog/app/(dynamic)/ossu/archive/[lectureName]/[courseName]/layout.tsx
+++ apps/blog/app/(dynamic)/ossu/archive/[lectureName]/[courseName]/layout.tsx
@@ -1,5 +1,6 @@
 import { slugParams } from "@/constants/params";
-import { Breadcrumb as BreadcrumbUI, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbSeparator } from "@workspace/ui/components/breadcrumb";
+import { BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbSeparator, Breadcrumb as BreadcrumbUI } from "@workspace/ui/components/breadcrumb";
+import { cn } from "@workspace/ui/lib/utils";
 import { Link } from "next-view-transitions";
 
 /** route group이 달라서 부모로부터 static param을 받을 수 없음 */
@@ -22,29 +23,29 @@ export default async function Layout({
   children: React.ReactNode;
   params: Promise<{ lectureName: string; courseName: string }>;
 }>) {
-  const { lectureName, courseName } = await params;
+  const { lectureName } = await params;
 
   return (
     <section className="grid grid-cols-[minmax(0.875rem,_1fr)_minmax(auto,_708px)_minmax(0.875rem,_1fr)] sm:grid-cols-[minmax(96px,_3fr)_minmax(auto,_708px)_minmax(96px,_1fr)] pt-6 pb-[10vh] sm:pt-[80px] sm:pb-[20vh] w-full transition-all">
       <aside className="hidden lg:block sticky top-[80px] left-3 border-y-2 w-[180px] h-fit">
         <dl className="border-b-[1px] border-muted px-[7px]">
           <dd className="py-[6px]">
-            <Breadcrumb lectureName={lectureName} courseName={courseName} />
+            <Breadcrumb lectureName={lectureName} />
           </dd>
         </dl>
       </aside>
       <article className="col-start-2 min-w-0">
-        <Breadcrumb lectureName={lectureName} courseName={courseName} />
+        <Breadcrumb lectureName={lectureName} className="lg:hidden text-muted-foreground mb-1" />
         {children}
       </article>
     </section>
   );
 }
 
-const Breadcrumb = ({ lectureName, courseName }: { lectureName: string; courseName: string }) => {
+const Breadcrumb = ({ lectureName, className }: { lectureName: string; className?: string }) => {
   return (
     <BreadcrumbUI>
-      <BreadcrumbList className={`text-inherit font-semibold text-xs`}>
+      <BreadcrumbList className={cn(`text-inherit font-semibold text-xs`, className)}>
         <BreadcrumbItem>
           <BreadcrumbLink asChild>
             <Link href={`/ossu/archive`}>ossu</Link>
@@ -54,12 +55,6 @@ const Breadcrumb = ({ lectureName, courseName }: { lectureName: string; courseNa
         <BreadcrumbItem>
           <BreadcrumbLink asChild>
             <Link href={`/ossu/archive/${lectureName}`}>{lectureName}</Link>
-          </BreadcrumbLink>
-        </BreadcrumbItem>
-        <BreadcrumbSeparator />
-        <BreadcrumbItem>
-          <BreadcrumbLink asChild>
-            <Link href={`/ossu/archive/${lectureName}/${courseName}`}>{courseName}</Link>
           </BreadcrumbLink>
         </BreadcrumbItem>
       </BreadcrumbList>
